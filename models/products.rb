b29b25785f31a97a0609fa564c64007a70c9b67e
Rossini fonseca <rossinidrum@outlook.com>
2024-02-03 12:47:22 -0300

    feat: Cria metodo 'store_product' para salvar um novo produto no banco de dados e adiciona validações

--- models/products.rb
+++ models/products.rb
@@ -1,5 +1,21 @@
-require './database/connection'
+require '../database/connection'
 
-class Products_model< ActiveRecord::Base
+class Products< ActiveRecord::Base
+
+
+  validates :name, presence: {message: 'O nome não pode estar em branco'}
+  validates :description, presence: { message: "A descrição não pode estar em branco" }
+  validates :price, presence:  { message: "O preço não pode estar em branco" }, numericality: { greater_than_or_equal_to: 0, message: "O preço deve ser maior ou igual a zero" }
+  validates :quantity, presence: { message: "A quantidade não pode estar em branco" }, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: "A quantidade deve ser um número inteiro maior ou igual a zero" }
+
+
+  def store_product(data)
+    begin
+      self.attributes = data
+      save!
+    rescue ActiveRecord::RecordInvalid => e
+      raise ArgumentError, e.message
+    end
+  end
 
 end
